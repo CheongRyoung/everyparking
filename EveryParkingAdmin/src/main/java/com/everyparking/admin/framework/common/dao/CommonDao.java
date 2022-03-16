5d50bdabd8d4e9bc2d956e81284e6154756804c8
CheongRyoung <0201kchm0224@gmail.com>
2022-03-16 22:13:09 +0900

    팀프로젝트 합본 수정 및 셋업 진행 중

--- EveryParkingAdmin/src/main/java/com/everyparking/admin/framework/common/dao/CommonDao.java
+++
@@ -1,9 +0,0 @@
-package com.everyparking.admin.framework.common.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-@Mapper
-@Repository
-public class CommonDao {
-}
--- EveryParkingUser/src/main/java/com/everyparking/user/framework/common/dao/CommonDao.java
+++
@@ -1,9 +0,0 @@
-package com.everyparking.user.framework.common.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-@Mapper
-@Repository
-public interface CommonDao {
-}
